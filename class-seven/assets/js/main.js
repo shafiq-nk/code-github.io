79bc6ab0cafdfb8e430dd4395fddf343291f812f
shafiq-nk <newkaliboy@yahoo.com>
2017-05-15 13:34:40 +0600

    class-seven

---
+++ class-seven/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+   
+            $(".stuff-list").owlCarousel({
+                items: 4,
+                autoplay: false,
+                margin: 20,
+            });
+
+
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
