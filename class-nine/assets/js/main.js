189004da71c1007405f2f8f6a1310333b3a08bd7
shafiq-nk <newkaliboy@yahoo.com>
2017-05-15 13:42:47 +0600

    class-nine

---
+++ class-nine/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".client-testimonial-carousel").owlCarousel({
+           items: 3,
+            margin: 30,
+            nav: false,
+            dots: true,
+            loop: true
+        });
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
