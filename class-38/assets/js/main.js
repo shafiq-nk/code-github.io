3d97adf2ebbed0fce0b69d77534297e06494a67d
shafiq-nk <newkaliboy@yahoo.com>
2017-05-24 05:57:01 +0600

    class-38

---
+++ class-38/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            dots: true,
+            nav: true,
+            autoplay: false,
+            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+            loop: true
+        });
+        
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
