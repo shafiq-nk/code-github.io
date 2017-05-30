6c5fd8d1b4f67c67e42eb70bfd425585b39a9747
shafiq-nk <newkaliboy@yahoo.com>
2017-05-30 18:26:24 +0600

    class-50-exam

---
+++ class-50-exam/assets/js/main.js
@@ -0,0 +1,31 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".testimonial-slide").owlCarousel({
+            items: 1,
+            nav: false,
+            dots: true,
+            autoplay: true,
+            loop: true,
+            stopOnHover: true,
+            touchDrag: false,
+            
+        });
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
