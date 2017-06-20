2a7f7a40024df01d701ff561b5988619416f1b8a
shafiq-nk <newkaliboy@yahoo.com>
2017-06-20 08:33:16 +0600

    class-57-testimonial

---
+++ class-57-testimonial/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".list-author-name").perfectScrollbar({
+            wheelSpeed: 3,
+            maxScrollbarLength: 80
+        });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
