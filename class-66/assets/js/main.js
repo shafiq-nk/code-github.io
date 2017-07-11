bbde7bfc187c9ccd48b1788d40ae79fdc5fcdc31
shafiq-nk <newkaliboy@yahoo.com>
2017-07-11 09:20:10 +0600

    class-66

---
+++ class-66/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+/*
+      $(window).scroll(function() {
+         var scroll = $(window).scrollTop();
+          if (scroll >= 170){
+              $(".sticky-area").addClass("is-stick")
+          }
+          else {
+              $(".sticky-area").removeClass("is-stick")
+          }
+      });*/
+
+        $(".sticky-area").sticky();
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
