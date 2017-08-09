4a144f1695e9e67e422d9a2ad845a1fa793c4e04
shafiq-nk <newkaliboy@yahoo.com>
2017-08-09 10:24:48 +0600

    class-72

---
+++ class-72/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        // Counter up
+        $('.counter-number').counterUp({
+            delay: 10,
+            time: 1000
+        });
+        
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
