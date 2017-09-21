126e291247b68f12de57a48dc8318daddde51909
shafiq-nk <newkaliboy@yahoo.com>
2017-09-22 01:38:05 +0600

    class-75

---
+++ class-75/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $('#bar1').barfiller({
+            barColor: "#00ffcc"
+        });  
+        $('#bar2').barfiller({
+            barColor: "#00fcfc"
+        });;  
+        $('#bar3').barfiller({
+            barColor: "#000dcc"
+        });;     
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
