1b76afc32ffbc95fc9ea2e4ecb0292899bdf4e9d
shafiq-nk <newkaliboy@yahoo.com>
2017-10-14 09:38:13 +0600

    mailchimp

---
+++ mailchimp/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $('.mailchimp-form').ajaxChimp({
+            url: 'https://netlify.us17.list-manage.com/subscribe/post?u=86367ad7431439be1d97668a0&amp;id=117f99e774'
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
