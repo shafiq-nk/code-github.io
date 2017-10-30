781fc3863ba5a1fc9c3d2d8b1c26bcc34e1ca321
shafiq-nk <newkaliboy@yahoo.com>
2017-10-30 15:07:14 +0600

    class-82

---
+++ class-82/assets/js/main.js
@@ -0,0 +1,31 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+   
+        $(".fueature-project").owlCarousel({
+            items: 1,
+            nav: false,
+            dots: true,
+            loop:true,
+            autoplay: true
+        });
+
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
