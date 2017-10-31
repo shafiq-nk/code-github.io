36b68904069e09346787ef628602c705a49845da
shafiq-nk <newkaliboy@yahoo.com>
2017-10-31 19:47:37 +0600

    class-85

---
+++ class-85/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+  
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            nav: true,
+            dots: false,
+            loop:true,
+            autoplay: false,
+            navText: ['<i class="fa fa-arrow-left"></i>', '<i class="fa fa-arrow-right"></i>']
+        });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
