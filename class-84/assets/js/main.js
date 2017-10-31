fc5f6374a895f0f38453b70fd340da899637d3f3
shafiq-nk <newkaliboy@yahoo.com>
2017-10-31 12:22:32 +0600

    class-84

---
+++ class-84/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $('.flip-hover').hover(function(){
+            $(this).addClass('flip');
+        },function(){
+            $(this).removeClass('flip');
+        });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
