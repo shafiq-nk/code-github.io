d1da62b952dbff5cc5b87dbc7f93577c78c027f6
shafiq-nk <newkaliboy@yahoo.com>
2017-11-17 18:09:14 +0600

    class-88

--- class-88/assets/js/main.js
+++ class-88/assets/js/main.js
@@ -3,11 +3,13 @@
 
     jQuery(document).ready(function($){
 
-
-        $(".embed-responsive iframe").addClass("embed-responsive-item");
-        $(".carousel-inner .item:first-child").addClass("active");
-        
-        $('[data-toggle="tooltip"]').tooltip();
+        $(".slider-area").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            dots: true,
+            nav: false
+        });
 
 
 
