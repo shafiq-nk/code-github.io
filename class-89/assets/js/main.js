725bfeb85f548d4547956faa963ebd6f1d364970
shafiq-nk <newkaliboy@yahoo.com>
2017-11-17 18:12:00 +0600

    class-89

---
+++ class-89/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".slider-area").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            dots: true,
+            nav: false
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
